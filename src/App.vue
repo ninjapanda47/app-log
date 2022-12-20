7390048b86b5f199c29905ab0b661531d2ce071a
Sandi <sandi.c.gau@gmail.com>
2022-12-19 20:05:01 -0600

    Mock up login page

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app theme="dark">
+    <v-app-bar title="Sandi's Job Tracker"
+      ><template v-slot:prepend>
+        <v-app-bar-nav-icon
+          @click.stop="drawer = !drawer"
+        ></v-app-bar-nav-icon> </template
+    ></v-app-bar>
+    <v-navigation-drawer v-model="drawer" temporary>
+      <v-list density="compact" nav>
+        <v-list-item v-for="item in items"
+          :prepend-icon="item.icon"
+          :title="item.title"
+        ></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="main-wrapper">
+    <router-view></router-view>
+    </v-main>
+    <v-footer>
+      <v-row class="justify-center">
+        <v-col class="text-center">
+          {{ new Date().getFullYear() }} — <strong>Made with <v-icon icon="mdi-heart-outline" color="pink"></v-icon> by Sandi</strong>
+
+        </v-col>
+      </v-row>
+    </v-footer>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+
+export default defineComponent({
+  components: {},
+  data() {
+    return {
+      drawer: true,
+      username: "Sandi",
+      items: [
+        { title: "Tracker", icon: "mdi-folder-table" },
+        { title: "Log Out", icon: "mdi-logout" },
+      ],
+    };
+  },
+});
+</script>
+<style>
+.main-wrapper {
+  min-height: 600px;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,62 @@
+<template>
+    <v-responsive >
+      <v-container>
+        <v-row align="center">
+          <v-col>
+            <v-card width="400"
+                     class="mx-auto mt-10 ">
+              <v-card-title class="text-center my-4 text-lg-h4">Login</v-card-title>
+              <v-form class="mx-5 text-center">
+                <v-text-field v-model="email" label="Email" type="email" required></v-text-field>
+                <v-text-field
+                    v-model="password"
+                    label="Password"
+                    type="password"
+                    required
+                ></v-text-field>
+                <v-btn
+                    color="success"
+                    class="mx-auto mb-5"
+                    @click="validate"
+                >
+                  Submit
+                </v-btn>
+              </v-form>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-responsive>
+</template>
+<script lang="ts">
+import { useVuelidate } from "@vuelidate/core";
+import { required, email } from "@vuelidate/validators";
+import { defineComponent } from "vue";
+export default defineComponent({
+  setup() {
+    return { v$: useVuelidate() };
+  },
+  data() {
+    return {
+      email: null,
+      password: null,
+    };
+  },
+  validations() {
+    return {
+      email: { required, email },
+      password: { required },
+    };
+  },
+  methods: {
+    async validate() {
+      const valid = await this.v$.$validate();
+      if (valid) {
+        // login via auth0
+      } else {
+
+      }
+    },
+  },
+});
+</script>
