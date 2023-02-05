7ae033bca9075c2858ab9186ff15cae54d53ef0a
Sandi <sandi.c.gau@gmail.com>
2023-02-05 17:21:57 -0600

    add date filter to report view

--- src/views/ReportsView.vue
+++ src/views/ReportsView.vue
@@ -1,38 +1,56 @@
 <template>
-    <v-card class="report">
-      <v-row class="my-5">
-        <v-col col="6"></v-col>
-        <v-col col="6" class="d-flex justify-center"
-        >From: <datepicker
-            v-model="applicationStore.dateFilter.startDate"
-            typeable
-            class="dateFilter"
-        /> To: <datepicker
-            v-model="applicationStore.dateFilter.endDate"
-            typeable
-            class="dateFilter"
-        /></v-col
-        >
-      </v-row>
-      <v-row class="mx-5">
-        <v-col col="4" class="d-flex justify-center"><v-card class="scoreCard" variant="outlined" color="primary"><h2>Total Application: {{ applicationStore.report.total }}</h2></v-card></v-col>
-        <v-col col="4" class="d-flex justify-center"> <v-card
-            class="scoreCard"
-            color="primary"
-        ><h2 class="my-auto">Application In Process: {{ applicationStore.report.inProcessCount }}</h2></v-card></v-col>
-        <v-col col="4" class="d-flex justify-center"><v-card
-            class="scoreCard"
-            color="warning"
-            theme="dark"
-        ><h2>Rejection: {{ applicationStore.report.rejectedCount }}</h2></v-card></v-col>
-      </v-row>
+  <v-card class="report">
+    <v-row class="my-5">
+      <v-col col="6"></v-col>
+      <v-col col="6" class="d-flex justify-center"
+        >From:
+        <datepicker
+          v-model="applicationStore.dateFilter.startDate"
+          typeable
+          class="dateFilter"
+        />
+        To:
+        <datepicker
+          v-model="applicationStore.dateFilter.endDate"
+          typeable
+          class="dateFilter"
+        /><v-btn
+          prepend-icon="mdi-magnify"
+          color="info"
+          @click="applicationStore.fetchAndSetReport"
+          >Filter</v-btn
+        ></v-col
+      >
+    </v-row>
+    <v-row class="mx-5">
+      <v-col col="4" class="d-flex justify-center"
+        ><v-card class="scoreCard" variant="outlined" color="primary"
+          ><h2>
+            Total Application: {{ applicationStore.report.total }}
+          </h2></v-card
+        ></v-col
+      >
+      <v-col col="4" class="d-flex justify-center">
+        <v-card class="scoreCard" color="primary"
+          ><h2 class="my-auto">
+            Application In Process: {{ applicationStore.report.inProcessCount }}
+          </h2></v-card
+        ></v-col
+      >
+      <v-col col="4" class="d-flex justify-center"
+        ><v-card class="scoreCard" color="warning" theme="dark"
+          ><h2>
+            Rejection: {{ applicationStore.report.rejectedCount }}
+          </h2></v-card
+        ></v-col
+      >
+    </v-row>
   </v-card>
-
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
-import { useApplicationStore} from "@/stores/application";
-import type { AppInfo } from "@/stores/application"
+import { useApplicationStore } from "@/stores/application";
+import type { AppInfo } from "@/stores/application";
 import dayjs from "dayjs";
 import Datepicker from "vue3-datepicker";
 
@@ -42,20 +60,16 @@ export default defineComponent({
     return { applicationStore };
   },
   components: {
-    Datepicker
+    Datepicker,
   },
   async mounted() {
     await this.applicationStore.fetchAndSetReport();
   },
   data() {
-    return {
-    };
+    return {};
   },
-  methods: {
-
-  },
+  methods: {},
 });
-
 </script>
 <style>
 .report {
